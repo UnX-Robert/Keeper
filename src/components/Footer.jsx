3e4f709ca4f09e19b99e38dce0470ae2dd19a880
Robert <antonrobert73@gmail.com>
2022-01-24 10:53:06 +0200

    Used conditional rendering, useState hook and some external components to add some nice features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 function Footer() {
   const year = new Date().getFullYear();
-
   return (
     <footer>
       <p>Copyright ⓒ {year}</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import StarsIcon from '@mui/icons-material/Stars';
 
 function Header() {
-  return (
-    <header>
-      <h1>Keeper</h1>
-    </header>
-  );
+	return (
+		<header>
+			<h1>
+				<StarsIcon /> Keeper
+			</h1>
+		</header>
+	);
 }
 
 export default Header;
